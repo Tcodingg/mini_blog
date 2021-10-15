4ca4a3f08cf45b9ade94990ef1c6bae700193f30
Tcodingg <princetlive@yahoo.com>
2021-10-15 14:40:02 -0400

    change the mini blog container display to grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import MiniBlog from "./MiniBlog/MiniBlog";
 import Header from "./Header/Header";
+import "./App.css";
 
 const App = () => {
    return (
-      <div>
+      <div className="app">
          <Header />
          <MiniBlog />
       </div>
