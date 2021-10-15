fd89038cbfef7ca3dfb6d4105f73204c7b661248
Tcodingg <princetlive@yahoo.com>
2021-10-15 16:18:22 -0400

    add minor styling;

--- client/src/Posts/Post.js
+++ client/src/Posts/Post.js
@@ -10,13 +10,13 @@ const Post = () => {
          <img src={camaro_2021} alt="" />
          <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. In
-            architecto praesentium molestiae corrupti iste dolor. A est labore
-            illum error!
+            architecto praesentium is molestiae corrupti iste dolor. A est
+            labore illum error!
          </p>
          <div className="icons-container">
             <div className="thumbup-counter icons">
                <IoMdThumbsUp className="thumb-up " />
-               <p>10</p>
+               <p style={{ fontSize: "10px" }}>10</p>
             </div>
             <MdDelete className="delete icons" />
          </div>
